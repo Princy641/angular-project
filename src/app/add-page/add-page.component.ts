eeac5b8d44f721b4d6b39c208ee512a52066205a
Princy Sharma <princyapcps@gmail.com>
2022-01-06 15:47:46 +0530

    naviagtes to list once added

--- src/app/add-page/add-page.component.ts
+++ src/app/add-page/add-page.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,7 +11,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class AddPageComponent implements OnInit {
   userForm!: FormGroup;
-  constructor(private _formBilder: FormBuilder){}
+  constructor(private _formBilder: FormBuilder, 
+    private http:HttpClient,
+    private router: Router){}
   ngOnInit(){
     this.userForm = this._formBilder.group({
       productName:[' ',[Validators.required]],
@@ -25,7 +29,11 @@ export class AddPageComponent implements OnInit {
     
 onSubmit(){
   console.log(this.userForm.value);
-
+  this.http.post('http://testapi.techriff.in/api/open/products', this.userForm.value)
+    .subscribe(data=>{
+      console.log("Successfully added product",data);
+      this.router.navigate(['/List']);
+    });
 }
 
 
